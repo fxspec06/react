d816fab6cacc95e76eae5c3ea0d80755e305cf81
fxspec06 <bshado@charter.net>
2020-07-30 19:06:12 -0400

    first
    first commit

---
+++ precache-manifest.8d231154e9955a7584bef281bf3ba646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c83296d86ae3f7159013aae200612e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f10e655d93d2a5cdf842",
+    "url": "/static/css/main.50b81d5d.chunk.css"
+  },
+  {
+    "revision": "70c81dc9ecdf3be7ebe0",
+    "url": "/static/js/2.6771a295.chunk.js"
+  },
+  {
+    "revision": "f10e655d93d2a5cdf842",
+    "url": "/static/js/main.4fa860f3.chunk.js"
+  },
+  {
+    "revision": "8339aa2ba8bf82f99f91",
+    "url": "/static/js/runtime-main.27512ada.js"
+  },
+  {
+    "revision": "9eec70bce4d225e9277a6ad8b6cfd892",
+    "url": "/static/media/bryan.9eec70bc.jpg"
+  },
+  {
+    "revision": "7a0c012e0a82bf3832eaff69e34ca210",
+    "url": "/static/media/pam.7a0c012e.jpg"
+  }
+]);
